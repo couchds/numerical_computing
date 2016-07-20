611c8ffdee5294cc408e5b5526b1afa033dd2c22
Daniel Couch <couchds@email.wofford.edu>
2016-07-20 23:36:11 +0100

    some work on interface

--- interface.c
+++ interface.c
@@ -8,7 +8,19 @@ User inputs the parameters of the differential equation they are solving.
 
 #include "interface.h"
 
-void display_menu(){
+int run(void){
+	FileStructure *OutputFile;
+	int *valid_choices = malloc(5 * sizeof(int));
+}
 
+int user_interface(FileStructure &FileS, int *choices){
+        int i = 0;
+        int choice = 0;
+        // Choice for integer must be between 1 and 7.
+        while (choice <= 7 && choice > 0 && sizeof(choice) == 2){
+                menu();
+                scanf("%d", &choice);
+	} 
+       return 0;
 }
 
--- interface.h
+++ interface.h
@@ -5,5 +5,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* Data that will be written to file output. */
+typedef struct FileStructure{
+    // represents the type of BC.
+    int bc_type;
+    // represents the type of IC.
+    int ic_type;
+    // represents the type of PDE.
+    int equation_type;
+    // represents the type of solution.
+    int solution_method;
+    // how long to run
+    double time;
+    // Implement this...
+    int *constants;
+    // what to name the output file
+    char *file_name;
+} FileStructure;
+
+int run(void);
+int user_interface(FileStructure &FileS, int *choices);
+int menu(FileStructure FileStruct);
+
 #endif /* INTERFACE_H */
 
